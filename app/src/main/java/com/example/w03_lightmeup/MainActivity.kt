2f87816c7dc58cdc5d055e1fe6ac423ce46bad8e
Dilni-DS113 <103235133+Dilni-DS113@users.noreply.github.com>
2022-08-26 16:44:41 +1000

    rotate enabled and saved state working

--- app/src/main/java/com/example/w03_lightmeup/MainActivity.kt
+++ app/src/main/java/com/example/w03_lightmeup/MainActivity.kt
@@ -2,17 +2,25 @@
 
 package com.example.w03_lightmeup
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
-    var state = R.drawable.ic_home_black_24dp
+   var state = R.drawable.ic_home_black_24dp
+    @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val home = findViewById<ImageView>(R.id.imagesalternative)
+
+        savedInstanceState?.let {
+            state = it.getInt("home")
+            home.setImageDrawable(getDrawable(state))
+        }
+
             home.setOnClickListener {
                 state = when(state)
                 {
@@ -23,13 +31,10 @@ class MainActivity : AppCompatActivity() {
             home.setImageDrawable(getDrawable(state))
         }
 
-
-
-
-
-
-
     }
 
-
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("home",state)
+    }
 }
