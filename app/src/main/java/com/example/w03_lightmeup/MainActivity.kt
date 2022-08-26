c072935e83645243e451b039566d9aa4896d08bc
Dilni-DS113 <103235133+Dilni-DS113@users.noreply.github.com>
2022-08-26 17:21:26 +1000

    view model created

--- app/src/main/java/com/example/w03_lightmeup/MainActivity.kt
+++ app/src/main/java/com/example/w03_lightmeup/MainActivity.kt
@@ -6,35 +6,55 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModel
 
 class MainActivity : AppCompatActivity() {
-   var state = R.drawable.ic_home_black_24dp
+
+    val imageViewModel: ImageViewModel by viewModels()
+
+
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val home = findViewById<ImageView>(R.id.imagesalternative)
+        home.setImageDrawable(getDrawable(imageViewModel.state))
 
-        savedInstanceState?.let {
-            state = it.getInt("home")
-            home.setImageDrawable(getDrawable(state))
-        }
+//        savedInstanceState?.let {
+//            state = it.getInt("home")
+//            home.setImageDrawable(getDrawable(state))
+      //  }
 
             home.setOnClickListener {
-                state = when(state)
-                {
-                    R.drawable.ic_home_black_24dp->R.drawable.ic_settings_black_24dp
-                    R.drawable.ic_settings_black_24dp -> R.drawable.ic_home_black_24dp
-                    else ->R.drawable.ic_home_black_24dp
-                }
-            home.setImageDrawable(getDrawable(state))
+                imageViewModel.nextImage()
+//                state = when(state)
+//                {
+//                    R.drawable.ic_home_black_24dp->R.drawable.ic_settings_black_24dp
+//                    R.drawable.ic_settings_black_24dp -> R.drawable.ic_home_black_24dp
+//                    else ->R.drawable.ic_home_black_24dp
+//                }
+            home.setImageDrawable(getDrawable(imageViewModel.state))
+
         }
 
     }
 
-    override fun onSaveInstanceState(outState: Bundle) {
-        super.onSaveInstanceState(outState)
-        outState.putInt("home",state)
+//    override fun onSaveInstanceState(outState: Bundle) {
+//        super.onSaveInstanceState(outState)
+//        outState.putInt("home",state)
+//    }
+}
+
+class ImageViewModel: ViewModel(){
+    var state = R.drawable.ic_home_black_24dp
+
+    fun nextImage(){
+        state = when(state) {
+            R.drawable.ic_home_black_24dp->R.drawable.ic_settings_black_24dp
+            R.drawable.ic_settings_black_24dp -> R.drawable.ic_home_black_24dp
+            else ->R.drawable.ic_home_black_24dp
+        }
     }
 }
